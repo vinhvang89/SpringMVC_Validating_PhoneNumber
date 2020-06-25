7d1c0a31af00b341940e94a1546502b21867476e
vinhvang89 <61720675+vinhvang89@users.noreply.github.com>
2020-06-25 11:01:20 +0700

    new

---
+++ src/main/java/Spaceship/Init.java
@@ -0,0 +1,20 @@
+package Spaceship;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Init extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{ApplicationConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
